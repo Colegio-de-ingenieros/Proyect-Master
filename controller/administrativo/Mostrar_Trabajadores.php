94031fefcc78220c3e07fcad53af8013f8e07bd7
Retzat <ret.hatsu@gmail.com>
2023-03-20 17:08:05 -0600

    Mostrar no funcional

---
+++ controller/administrativo/Mostrar_Trabajadores.php
@@ -0,0 +1,9 @@
+<?php
+$server = "localhost"
+$username = "AdminCISCIG"
+$password = "ColegioCISCIG2023."
+$dbname = "colegiociscig"
+
+$db=new mysqli($server,$username,$password,$dbname);
+
+?>
--- controller/administrativo/js/Mostrar_Trabajadores.js
+++ controller/administrativo/js/Mostrar_Trabajadores.js
@@ -1,6 +1,41 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const search=document.querySelector('#search');
+    const tabla=document.querySelector('#tabla_Trabajador tbody');
+    const result=document.querySelector('#resultContainer');
+    const errors=document.querySelector('.errorsContainer');
+    let criterio = ''
+    if(search){
+        search.addEventListener('input', event =>{
+            criterio = event.target.value.toUpperCase()
+            mostrarTrabajadores()
+        })
+    }
+    //enviar peticion usando fetch
+    const buscarTrabajadores = async () =>{
+        let buscarTrabajadores=new FormData()
+        buscarTrabajadores.append('criterio', criterio)
+        try{
+            const response=await fetch('../../model/Mostrar_Trabajadores.php',{
+                method: 'POST',
+                body: buscarTrabajadores
+            })
+            return response.json()
+        }catch(error){
+            alert(`${'Error al buscar los trabajadores'}${error.message}`)
+            console.log(error)
+        }
+    }
+    //funcion para mostrar los datos
+    const mostrarTrabajadores = () =>{
+        buscarTrabajadores()
+        .then(dataResults =>{
+            console.log(dataResults)
+        })
+    }
 
+});
 
-let trabajador=[
+/*let trabajador=[
     {rfc:"MIML970715L19",nombre:"Leobardo", apaterno:"Miramontes", amaterno:"Ibarra",correo:'led.tesmur@gmail.com',telefono:'449 123 4567'},
     {rfc:"MIML970715Q34",nombre:"Leobardo", apaterno:"Mendoza", amaterno:"Ibarra",correo:'led.tesmur@gmail.com',telefono:'449 123 4567'},
     {rfc:"MIML970715FCV",nombre:"Leobardo", apaterno:"Mendoza", amaterno:"Ibarra",correo:'led.tesmur@gmail.com',telefono:'449 123 4567'},
@@ -23,4 +58,4 @@ let crearTabla = function(lista){
     }
     return stringTabla;
 };
-document.getElementById("tabla_Trabajador").innerHTML= crearTabla(trabajador);
+document.getElementById("tabla_Trabajador").innerHTML= crearTabla(trabajador);*/
