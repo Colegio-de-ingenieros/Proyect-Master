58cb5de8d7236f70d01a9b6a401893bc3bb93165
Retzat <ret.hatsu@gmail.com>
2023-03-19 22:54:08 -0600

    Registro trabajadores terminado?

---
+++ controller/administrativo/Registro_Trabajadores.php
@@ -0,0 +1,42 @@
+<?php
+include_once('../../model/Reg_Trabajadores.php');
+
+class RegistroTrabajadores{
+    private $obj, $rfc, $nombre, $apaterno, $amaterno, $correo, $telefono, $pass;
+
+    //inicializa los valores que ocupan las demas funciones
+    function instancias(){
+        $this->obj = new NuevoTrabajador();
+        $this->obj->conexion();
+        $this->rfc = $_POST["caja_rfc"];
+        $this->nombre = $_POST["caja_nombre"];
+        $this->apaterno = $_POST["caja_ap_paterno"];
+        $this->amaterno = $_POST["caja_ap_materno"];
+        $this->correo = $_POST["caja_correo"];
+        $this->telefono = $_POST["caja_telefono"];
+        $this->pass = $_POST["caja_contra"];
+
+    }
+
+    //manda a llamar al archivo de model para meter los datos a la base
+    function insertar(){
+        $this->obj->insertar($this->rfc, $this->nombre, $this->apaterno, $this->amaterno, $this->correo, $this->telefono, $this->pass);
+
+        //verifica que los datos se insertarin en la base de datos
+        $resultados = $this->obj->buscarPorRFC($this->rfc);
+
+        if($resultados == true){
+            echo json_encode('exito');
+        }
+
+        else{
+            echo json_encode('Ha ocurrido un error al conectar con la base de datos');
+        }
+    }
+
+}
+
+$obj = new RegistroTrabajadores();
+$obj->instancias();
+
+?>
---
+++ model/Reg_Trabajadores.php
@@ -0,0 +1,46 @@
+<?php
+    include('../../config/Crud_bd.php');
+
+    class NuevoTrabajador{
+        private $base;
+
+        //crea un objeto del CRUD para hacer las consultas
+        function conexion(){
+            $this->base = new Crud_bd();
+            $this->base->conexion_bd();
+        }
+
+        //retorna true si el id que recibe ya esta en la base y false si no
+        function buscarPorRFC($rfc){
+            $querry = "SELECT * FROM trabajadores
+            WHERE RFCT = :rfc";
+
+            $arre = [":rfc"=>$rfc];
+
+            $resultados = $this->base->mostrar($querry, $arre);
+
+            if($resultados != null){
+                return true;
+            }
+
+            else{
+                return false;
+            }
+        }
+
+        //manda las consultas para insertar en las tablas de certificaciones internas e historicos
+        function insertar($nombre, $apat, $amat, $rfc, $correo, $telefono, $pass){
+            //consultas para la tabla de certificaciones internas
+            $q1 = "INSERT INTO trabajadores (RFCT,NombreT,ApePT,ApeMT,CorreoT,TelT,ContraT)
+            VALUES(:rfc, :nombre, :apat, :amat, :correo, :telefono, :pass)";
+            $a1= [":rfc"=>$rfc, ":nombre"=>$nombre, ":apat"=>$apat, ":amat"=>$amat, ":correo"=>$correo, ":telefono"=>$telefono, ":pass"=>$pass];
+            //acomoda todo en arreglos para mandarlos al CRUD
+            $querry = [$q1];
+            $parametros = [$a1];
+
+            $this->base->insertar_eliminar_actualizar($querry, $parametros);
+        }
+        
+    }
+
+?>
