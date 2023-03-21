7f083952877f4a6d077789cd9c1a8ac4df64097e
Ana-Isabel-Garcia <cuentaderespaldogit@gmail.com>
2023-03-21 11:30:41 -0600

    no importé el ajax :v

--- view/administrativo/Vista_Certificaciones.php
+++ view/administrativo/Vista_Certificaciones.php
@@ -1,27 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vizualización certificaciones</title>
-    <script src="https://kit.fontawesome.com/f8c41f1595.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../../public/css/style.css">
-    <link rel="stylesheet" href="../../public/css/administrativo/plantilla_admin.css">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Vizualización certificaciones</title>
+	<script src="https://kit.fontawesome.com/f8c41f1595.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="../../public/css/style.css">
+	<link rel="stylesheet" href="../../public/css/administrativo/plantilla_admin.css">
 	<link rel="stylesheet" href="../../public/css/administrativo/Vista_Certificaciones.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="../../controller/administrativo/js/Mostrar_Certificaciones.js"></script>
 </head>
+
 <body>
-    <header>
+	<header>
 		<div class="header_superior">
 			<div class="titulo">
-	  
-			  <div class="nombre_ventana">
-				<img class="logo_ciscig" src="../../public/img/LOGO_CISCIG-white.png" width="10px" alt="">
-				<h1 class="nombre_Ventana">Certificaciones</h1>
-			  </div>
-	  
+
+				<div class="nombre_ventana">
+					<img class="logo_ciscig" src="../../public/img/LOGO_CISCIG-white.png" width="10px" alt="">
+					<h1 class="nombre_Ventana">Certificaciones</h1>
+				</div>
+
 			</div>
-	  
+
 			<div class="boton-cerrar-session">
 				<button class="btn-cerrar-session btn">Cerrar sesión</button>
 			</div>
@@ -30,149 +34,149 @@
 		<!-- Menu Desplegable -->
 		<div class="container_menu">
 			<div class="menu">
-			  <input type="checkbox" id="check__menu">
-			  <label id="label__check" for="check__menu">
-				<i class="fa-sharp fa-solid fa-bars icon__menu"></i>
-			  </label>
-			  <nav>
-				<ul>
-	  
-				  <li>
-					<a href="#">
-					  <i class="fa-regular fa-user"></i>
-					  Usuarios
-					</a>
-	  
+				<input type="checkbox" id="check__menu">
+				<label id="label__check" for="check__menu">
+					<i class="fa-sharp fa-solid fa-bars icon__menu"></i>
+				</label>
+				<nav>
 					<ul>
-					  <li><a id="menuProducto1" href="#">Visualizar</a></li>
+
+						<li>
+							<a href="#">
+								<i class="fa-regular fa-user"></i>
+								Usuarios
+							</a>
+
+							<ul>
+								<li><a id="menuProducto1" href="#">Visualizar</a></li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="#">
+								<i class="fa-solid fa-user-tie"></i>
+								Trabajadores
+							</a>
+
+							<ul>
+								<li><a id="menuSucursal1" href="#">Registrar</a></li>
+								<li><a id="menuSucursal2" href="#">Visualizar</a></li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="#">
+								<i class="fa-solid fa-circle-check"></i>
+								Certificaciones
+							</a>
+
+							<ul>
+								<li><a id="menuSucursal1" href="#">Registrar</a></li>
+								<li><a id="menuSucursal2" href="#">Visualizar</a></li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="#">
+								<i class="fa-solid fa-chalkboard-user"></i>
+								Cursos
+							</a>
+
+							<ul>
+								<li><a id="menuSucursal1" href="#">Registrar</a></li>
+								<li><a id="menuSucursal2" href="#">Visualizar</a></li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="#">
+								<i class="fa-solid fa-clipboard-list"></i>
+								Proyectos
+							</a>
+
+							<ul>
+								<li><a id="menuSucursal1" href="#">Registrar</a></li>
+								<li><a id="menuSucursal2" href="#">Visualizar</a></li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="#">
+								<i class="fa-solid fa-person-chalkboard"></i>
+								Instructores
+							</a>
+
+							<ul>
+								<li><a id="menuSucursal1" href="#">Registrar</a></li>
+								<li><a id="menuSucursal2" href="#">Visualizar</a></li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="#">
+								<i class="fa-solid fa-chart-column"></i>
+								Seguimiento
+							</a>
+
+							<ul>
+								<li><a id="menuSucursal1" href="#">Registrar</a></li>
+								<li><a id="menuSucursal2" href="#">Visualizar</a></li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="#">
+								<i class="fa-solid fa-file"></i>
+								Pólizas
+							</a>
+
+							<ul>
+								<li><a id="menuSucursal1" href="#">Registrar</a></li>
+								<li><a id="menuSucursal2" href="#">Visualizar</a></li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="#">
+								<i class="fa-solid fa-file-lines"></i>
+								Reportes
+							</a>
+
+							<ul>
+								<li><a id="menuSucursal1" href="#">Individual</a></li>
+								<li><a id="menuSucursal2" href="#">General</a></li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="#">
+								<i class="fa-solid fa-user-gear"></i>
+								Servicios
+							</a>
+
+							<ul>
+								<li><a id="menuSucursal1" href="#">Visualizar</a></li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="#">
+								<i class="fa-solid fa-briefcase"></i>
+								Bolsa de trabajo
+							</a>
+
+							<ul>
+								<li><a id="menuSucursal1" href="#">Ofertas</a></li>
+							</ul>
+						</li>
 					</ul>
-				  </li>
-	  
-				  <li>
-					<a href="#">
-					  <i class="fa-solid fa-user-tie"></i>
-					  Trabajadores
-					</a>
-	  
-					<ul>
-					  <li><a id="menuSucursal1" href="#">Registrar</a></li>
-					  <li><a id="menuSucursal2" href="#">Visualizar</a></li>
-					</ul>
-				  </li>
-	  
-				  <li>
-					<a href="#">
-					  <i class="fa-solid fa-circle-check"></i>
-					  Certificaciones
-					</a>
-	  
-					<ul>
-					  <li><a id="menuSucursal1" href="#">Registrar</a></li>
-					  <li><a id="menuSucursal2" href="#">Visualizar</a></li>
-					</ul>
-				  </li>
-	  
-				  <li>
-					<a href="#">
-					  <i class="fa-solid fa-chalkboard-user"></i>
-					  Cursos
-					</a>
-	  
-					<ul>
-					  <li><a id="menuSucursal1" href="#">Registrar</a></li>
-					  <li><a id="menuSucursal2" href="#">Visualizar</a></li>
-					</ul>
-				  </li>
-	  
-				  <li>
-					<a href="#">
-					  <i class="fa-solid fa-clipboard-list"></i>
-					  Proyectos
-					</a>
-	  
-					<ul>
-					  <li><a id="menuSucursal1" href="#">Registrar</a></li>
-					  <li><a id="menuSucursal2" href="#">Visualizar</a></li>
-					</ul>
-				  </li>
-	  
-				  <li>
-					<a href="#">
-					  <i class="fa-solid fa-person-chalkboard"></i>
-					  Instructores
-					</a>
-	  
-					<ul>
-					  <li><a id="menuSucursal1" href="#">Registrar</a></li>
-					  <li><a id="menuSucursal2" href="#">Visualizar</a></li>
-					</ul>
-				  </li>
-	  
-				  <li>
-					<a href="#">
-					  <i class="fa-solid fa-chart-column"></i>
-					  Seguimiento
-					</a>
-	  
-					<ul>
-					  <li><a id="menuSucursal1" href="#">Registrar</a></li>
-					  <li><a id="menuSucursal2" href="#">Visualizar</a></li>
-					</ul>
-				  </li>
-	  
-				  <li>
-					<a href="#">
-					  <i class="fa-solid fa-file"></i>
-					  Pólizas
-					</a>
-	  
-					<ul>
-					  <li><a id="menuSucursal1" href="#">Registrar</a></li>
-					  <li><a id="menuSucursal2" href="#">Visualizar</a></li>
-					</ul>
-				  </li>
-	  
-				  <li>
-					<a href="#">
-					  <i class="fa-solid fa-file-lines"></i>
-					  Reportes
-					</a>
-	  
-					<ul>
-					  <li><a id="menuSucursal1" href="#">Individual</a></li>
-					  <li><a id="menuSucursal2" href="#">General</a></li>
-					</ul>
-				  </li>
-	  
-				  <li>
-					<a href="#">
-					  <i class="fa-solid fa-user-gear"></i>
-					  Servicios
-					</a>
-	  
-					<ul>
-					  <li><a id="menuSucursal1" href="#">Visualizar</a></li>
-					</ul>
-				  </li>
-	  
-				  <li>
-					<a href="#">
-					  <i class="fa-solid fa-briefcase"></i>
-					  Bolsa de trabajo
-					</a>
-	  
-					<ul>
-					  <li><a id="menuSucursal1" href="#">Ofertas</a></li>
-					</ul>
-				  </li>
-				</ul>
-			  </nav>
+				</nav>
 			</div>
 		</div>
 	</header>
 
 	<main>
-		<section class="section-main">
+		<section class="section-main" id="tablaResultado">
 			<div class="cabezera">
 				<h4 class="subtitulo-1">Visualización / Consulta</h4>
 			</div>
@@ -181,13 +185,15 @@
 			<form class="formulario-de-vista" id="formulario">
 				<div class="grupo-input">
 					<div class="input-form">
-						<input type="search" class="input-format2" placeholder="Search">				
+						<input type="search" class="input-format2" placeholder="Search" name="busqueda" id="busqueda">
 					</div>
 				</div>
 
 				<div class="table">
 					<div class="header_table">
-						<?php include('../../controller/administrativo/Mostrar_Certificaciones.php'); ?>
+
+						<?php //include('../../controller/administrativo/Mostrar_Certificaciones.php'); 
+						?>
 						<!---<table>
                             <thead>
                                 <tr>
@@ -202,10 +208,10 @@
 					</div>
 				</div>
 			</form>
-			<script src="js/Vali_Certificaciones.js"></script>
+
 		</section>
-	</main>	 
-    </div>
+	</main>
+	</div>
 </body>
-<script src="../../controller/administrativo/js/Registro_Certificaciones.js"></script>
+
 </html>
