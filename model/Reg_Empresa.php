8da0c76c8faf78705b4b6381e73b2a965d86febd
llamasdelacruz <tetillamas@gmail.com>
2023-03-27 12:45:30 -0600

    nombre de la tabla areaempresa

--- model/Reg_Empresa.php
+++ model/Reg_Empresa.php
@@ -98,7 +98,7 @@ class Alta_empresa extends Crud_bd{
        
     
 
-        $sql_area = "INSERT INTO areempresa (IdAreaEmp, NomEncArea, ApePEncArea, ApeMEncArea, TelFEncArea,"
+        $sql_area = "INSERT INTO areaempresa (IdAreaEmp, NomEncArea, ApePEncArea, ApeMEncArea, TelFEncArea,"
                 ."ExtenTelFEncArea, CorreoEncArea) VALUES (:id,:nom,:pa,:ma,:tel,:ext,:correo)";
         $parametros_area = [":id"=>$id, ":nom"=>$nombre,":pa"=>$paterno,
                             ":ma"=>$materno,":tel"=>$telefono,":ext"=>$extension,
