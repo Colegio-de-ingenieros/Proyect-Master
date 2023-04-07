15574a9d20873a946cc7b2039f100d6116516e4a
TchRui <Rui099098@gmail.com>
2023-04-07 13:56:06 -0600

    Ventana modal creada en bolsa de trabajo

---
+++ view/socio-asociado/js/modal.js
@@ -0,0 +1,11 @@
+const abrir_modal = document.getElementById('open');
+const cerrar_modal = document.getElementById('close');
+const modal = document.getElementById('modal-container');
+
+function mostrar_modal() {
+    modal.classList.remove('hide');
+    modal.classList.add('show');
+}
+function ocultar_modal() {
+    modal.classList.remove('show');
+}
