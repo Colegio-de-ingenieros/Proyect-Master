b90a6f680f01640a2f17496ba769a64893d3cb4d
ivan7351 <carlosivanmunoz3@gmail.com>
2023-04-08 18:47:28 -0600

    orden en pdf

--- controller/administrativo/Pdf_cursos.php
+++ controller/administrativo/Pdf_cursos.php
@@ -89,7 +89,71 @@
     $pdf->SetFont('Arial','',14);
 
     /* Crea un ciclo en donde se itere el tema y sus subtemas */
-    if ($datost){
+    if ($datost) {
+        $idtemasl = [];
+		$nomtemasl = [];
+        for ($i = 0; $i < count($datost); $i++) {
+            $tem = $datost[$i]["NomTema"];
+            $iden = $datost[$i]["IdTema"];
+
+
+            array_push($idtemasl, ((int)$iden));
+            array_push($nomtemasl, $tem);
+        }
+
+            //aplica ordenacion burbuja para ordenar los temas en numeros del menor al mayor
+        for ($i = 0; $i < count($idtemasl); $i++) {
+            for ($j = 0; $j < count($idtemasl); $j++) {
+                if ($idtemasl[$i] < $idtemasl[$j]) {
+                    $aux = $idtemasl[$i];
+                    $aux1 = $nomtemasl[$i];
+                    $nomtemasl[$i] = $nomtemasl[$j];
+                    $idtemasl[$i] = $idtemasl[$j];
+                    $idtemasl[$j] = $aux;
+                    $nomtemasl[$j] = $aux1;
+                }
+            }
+        }
+        
+        for ($i = 0; $i < count($idtemasl); $i++) {
+            $pdf->SetFont('Arial','B',16);
+            $pdf->Cell(0,10,utf8_decode($nomtemasl[$i]),0,1,'C');
+            //$respuesta .= '<h3 style="width: 500px; word-wrap: break-word;">'.$nomtemasl[$i] .'</h3><br>';
+            $datoss = $bd->s($tem,((string)$idtemasl[$i]));
+            $idsubtemasl = [];
+            $nomsubtemasl = [];
+            if ($datoss) {
+
+                for ($j = 0; $j < count($datoss); $j++) {
+                    $te = $datoss[$j]["NomSubT"];
+                    $idss = $datoss[$j]["IdSubT"]; 
+                    array_push($idsubtemasl, ((int)$idss));
+                    array_push($nomsubtemasl, $te);
+                }
+
+                //aplicar burbuja para ordenar los subtemas(id)
+                for ($is = 0; $is < count($idsubtemasl); $is++) {
+                    for ($js = 0; $js < count($idsubtemasl); $js++) {
+                        if ($idsubtemasl[$is] < $idsubtemasl[$js]) {
+                            $aux = $idsubtemasl[$is];
+                            $aux1 = $nomsubtemasl[$is];
+                            $nomsubtemasl[$is] = $nomsubtemasl[$js];
+                            $idsubtemasl[$is] = $idsubtemasl[$js];
+                            $idsubtemasl[$js] = $aux;
+                            $nomsubtemasl[$js] = $aux1;
+                        }
+                    }
+                }
+
+                for ($il = 0; $il < count($idsubtemasl); $il++) {
+                    $pdf->SetFont('Arial','',14);
+                    $pdf->Cell(0,10,utf8_decode($nomsubtemasl[$il]),0,1,'C');
+                    //$respuesta .= '<h4 style="width: 500px;">'.$nomsubtemasl[$il] .'</h4><br>';
+                }         
+            }
+        }
+    }
+   /*  if ($datost){
     for ($i = 0; $i < count($datost); $i++) {
         $tema = $datost[$i]["NomTema"];
         $ide = $datost[$i]["IdTema"];
@@ -105,7 +169,7 @@
             $pdf->Cell(0,10,utf8_decode($subtema),0,1,'C');
         }
     }
-}
+} */
 else {
     $pdf->SetFont('Arial','',14);
     $pdf->Cell(0,10,utf8_decode('No hay temas registrados'),0,1,'C');
