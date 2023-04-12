f53973802e750c369d296736d16e5f1f5cc074bd
FanySaLu <fanylulo@hotmail.com>
2023-04-12 14:47:55 -0600

    correciones modificar certificaciones

--- controller/administrativo/Get_Certificacion.php
+++ controller/administrativo/Get_Certificacion.php
@@ -16,7 +16,7 @@ $status = $resultado[0]["EstatusCertInt"];
 $precioG = $base->buscarUltimoPrecioG($idc);
 $precioA = $base->buscarUltimoPrecioA($idc);
 
-echo '<img src="data:image/jpeg;base64,' . base64_encode($logo) . '"width="100" height="100">';
+//echo '<img src="data:image/jpeg;base64,' . base64_encode($logo) . '"width="100" height="100">';
 
 ?>
 
@@ -28,5 +28,13 @@ echo '<img src="data:image/jpeg;base64,' . base64_encode($logo) . '"width="100" 
     document.getElementById("descripcion").value = "<?php echo $desc ?>";
     document.getElementById("precioAsoc").value = "<?php echo $precioA ?>";
     document.getElementById("precioGen").value = "<?php echo $precioG ?>";
+
+    const logoImg = document.createElement('img');
+    logoImg.src = "<?php echo 'data:image/jpeg;base64,' . base64_encode($logo) ?>";
+    logoImg.width = 100;
+    logoImg.height = 100;
+    const contenedor = document.getElementById('photo');
+    contenedor.appendChild(logoImg);
+
     //document.getElementById("inputLogo").value = "<?php echo '<img src="data:image/jpeg;base64,' . base64_encode($logo) . '"width="100" height="100">';?>";
 </script>
