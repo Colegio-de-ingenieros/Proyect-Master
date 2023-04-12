120da8fd05b1e0d674d2da39b428d40822218253
Retzat <ret.hatsu@gmail.com>
2023-04-12 15:21:14 -0600

    Modificacion en trabajadores

--- view/administrativo/Vista_trabajadores.php
+++ view/administrativo/Vista_trabajadores.php
@@ -1,4 +1,4 @@
-<script src="../../view/login/js/verificar_permiso_trabajador.js"></script>
+<!-- <script src="../../view/login/js/verificar_permiso_trabajador.js"></script> -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
