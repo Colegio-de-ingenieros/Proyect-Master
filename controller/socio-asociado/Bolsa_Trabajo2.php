59a6367cf8f71f8f1e5b491d5d7b5da00eacfa46
ivan7351 <carlosivanmunoz3@gmail.com>
2023-04-20 21:14:46 -0600

    mandar datos personales, del lugar donde vive y certificaciones externas

--- controller/socio-asociado/Bolsa_Trabajo2.php
+++ controller/socio-asociado/Bolsa_Trabajo2.php
@@ -7,7 +7,15 @@ if (isset ($_SESSION['usuario']  )&& isset($_SESSION['tipo_usuario'])){
 
     $objeto = new dato();
     $objeto->BD();
-    $datos = $objeto->usuario($tipo_usuario,$usuario);
+    $datos_personales = $objeto->usuario($tipo_usuario,$usuario);
+
+    $id = $datos_personales[0]['IdPerso'];
+    
+    $datos_de_lugar = $objeto->lugar($id);
+    $datos_de_certificaciones = $objeto->certificaciones($id);
+
+    $datos_auxiliar = array_merge($datos_personales,$datos_de_lugar);
+    $datos = array_merge($datos_auxiliar,$datos_de_certificaciones);
 
     header("Content-Type: application/json");
     echo json_encode($datos);
--- model/socio-asociado/Bolsa_Trabajo2.php
+++ model/socio-asociado/Bolsa_Trabajo2.php
@@ -11,15 +11,37 @@ class dato{
 
     function usuario($tipo,$correo){
         if ($tipo == 'socio'){
-            $consulta = "SELECT * FROM usuaperso WHERE binary(CorreoPerso) =  binary(:user)";
+            $consulta = "SELECT IdPerso,NomPerso,ApePPerso,ApeMPerso,FechaNacPerso,CorreoPerso,TelFPerso,TelMPerso,CallePerso
+             FROM usuaperso WHERE binary(CorreoPerso) =  binary(:user)";
             $parametros = [":user"=>$correo];
             $datos = $this->bd->mostrar($consulta,$parametros);
             return $datos;
        }
+    }
+    function lugar($id){
+        $consulta = "SELECT nomcolonia,nommunicipio,codpostal 
+        FROM usuaperso,persolugares,colonias,municipios
+        WHERE
+        binary(usuaperso.IdPerso) = binary(:id) 
+        and usuaperso.IdPerso = persolugares.IdPerso
+        and persolugares.IdColonia = colonias.IdColonia
+        and colonias.idmunicipio = municipios.idmunicipio";
+        $parametros = [":id"=>$id];
+        $datos = $this->bd->mostrar($consulta,$parametros);
+        return $datos;
 
     }
+    function certificaciones($id){
+        $consulta = "SELECT NomCerExt,OrgCerExt FROM usuaperso,persocertexterna,certexterna
+        WHERE 
+        binary(usuaperso.IdPerso) = binary(:id)
+        and usuaperso.IdPerso = persocertexterna.IdPerso
+        and persocertexterna.IdCertExt = certexterna.IdCerExt";
+        $parametros = [":id"=>$id];
+        $datos = $this->bd->mostrar($consulta,$parametros);
+        return $datos;
+    }
+
 }
-
-
 
 ?>
