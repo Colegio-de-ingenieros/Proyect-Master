dfe8b11e377610fb6d96c294c16032ec2ab1623b
Alan Trejo <shaggyman2500@gmail.com>
2023-04-23 14:11:53 -0600

    mas diseño

--- view/administrativo/Vista_Proyectos.php
+++ view/administrativo/Vista_Proyectos.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vizualización proyectos</title>
+    <title>Vizualización de proyectos</title>
     <script src="https://kit.fontawesome.com/f8c41f1595.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../../public/css/style.css">
     <link rel="stylesheet" href="../../public/css/administrativo/plantilla_admin.css">
