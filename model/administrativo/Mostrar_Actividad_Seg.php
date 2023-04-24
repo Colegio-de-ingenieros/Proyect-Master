d41eba3d1876289da357e91bcc354b181a9408e2
Naye <nayer9810@gmail.com>
2023-04-24 09:36:17 -0600

    Tabla Actividad Individual Ya Funciona

--- model/administrativo/Mostrar_Actividad_Seg.php
+++ model/administrativo/Mostrar_Actividad_Seg.php
@@ -425,43 +425,6 @@
 
             return $ejecucion;
         }
-
-//CONSULTAS PARA  MOSTRAR GASTOS E INGRESOS EN LA TABLA
-
-        public function consul_datos_tabla($id){
-            $lista=[];
-            $particSocios=$this->buscar_partic_socios($id);
-            $particEmp=$this->buscar_partic_empresas($id);
-            $particIns=$this->buscar_partic_instructores($id);
-            $resUno = array_merge($particSocios, $particEmp);
-            $resDos = array_merge($resUno, $particIns);
-            $id=[];
-            $nombre=[];
-
-            for ($i=0;$i<count($resDos);$i++){
-                array_push($id, $resDos[$i][0]);
-                array_push($nombre, $resDos[$i][1]);
-            }
-            $lista[0]=$id;
-            $lista[0]=$nombre;
-            return $lista;
-        }
-
-        public function buscar_socios_ingresos($id){
-            $this->conexion_bd();
-            $sql = "SELECT persoparticipa.IdParP, CONCAT_WS(' ', 'Asoc.', usuaperso.NomPerso, usuaperso.ApePPerso, usuaperso.ApeMPerso) as Nombre, controlingre.Idingre, sum(MontoIngre) as TotalIngre
-            FROM usuaperso, persoparticipa, seguimiento, persoingresos, controlingre
-            WHERE seguimiento.IdSeg = :id AND seguimiento.IdSeg = persoparticipa.IdSeg AND persoparticipa.IdPerso = usuaperso.IdPerso AND persoparticipa.IdParP = persoingresos.IdParP AND persoingresos.IdIngre = controlingre.IdIngre     
-            GROUP BY (persoparticipa.IdParP)
-            ORDER BY (usuaperso.NomPerso)";
-            $arre = [":id"=>$id];
-            $resultado = $this->mostrar($sql, $arre);
-            $this->cerrar_conexion();
-            return $resultado;
-        }
-
-
-
     }
 
 ?>
