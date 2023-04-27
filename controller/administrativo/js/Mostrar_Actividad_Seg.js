66d569868af43503b70a74fdbfe66b035f66773f
Naye <nayer9810@gmail.com>
2023-04-26 20:03:31 -0600

    Campso bloqueados

--- controller/administrativo/js/Mostrar_Actividad_Seg.js
+++ controller/administrativo/js/Mostrar_Actividad_Seg.js
@@ -72,14 +72,16 @@ function rellenar_datos(datos) {
         }
         if (dato.length != 0){
             dato.forEach(registro => {
-            var optionElement = document.createElement("option");
-            optionElement.value = registro[0];
-            optionElement.text = registro[1];
-            document.getElementById(campo).appendChild(optionElement);
+                var optionElement = document.createElement("option");
+                optionElement.value = registro[0];
+                optionElement.text = registro[1];
+                document.getElementById(campo).appendChild(optionElement);
             });
+
             if (i<4){
-            document.ready = document.getElementById(campo).value = '0';
+                document.ready = document.getElementById(campo).value = '0';
             }
+            document.getElementById(campo).disabled = false;
         } else {
             var optionElement = document.createElement("option");
             optionElement.value = "Vacio";
