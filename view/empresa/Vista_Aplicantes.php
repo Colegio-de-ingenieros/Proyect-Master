93c9434c0e580b757d3a4d9722c6f3b5a070a50b
Aldo6890 <aldocastillos433@gmail.com>
2023-04-28 22:53:59 -0600

    Correcion de nombres en ventanas.

--- view/empresa/Vista_Aplicantes.php
+++ view/empresa/Vista_Aplicantes.php
@@ -8,7 +8,7 @@ $id=$_GET['id'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vista aplicantes</title>
+    <title> Consultar aplicantes</title>
     <script src="https://kit.fontawesome.com/f8c41f1595.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	  <script src="../../controller/empresa/js/Mostrar_Aplicantes.js"></script>
