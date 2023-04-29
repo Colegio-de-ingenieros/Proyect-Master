49cfcc79e45a0303e46848765624a3683a19e915
LauRobles <robleslaura663l@gmail.com>
2023-04-29 06:44:42 -0600

    Correcciones proyectos

--- controller/administrativo/Get_Proyecto.php
+++ controller/administrativo/Get_Proyecto.php
@@ -1,34 +1,46 @@
 <?php
 //obtener el id
-$idp = $_GET["idp"];
-
-//llamar a los otros archivos que se ocupan
 include_once('../../model/administrativo/Mostrar_Proyectos.php');
-include_once('../../view/administrativo/Modi_Proyectos.html');
 
-//instancias la clase para buscar y traer los datos
-$base = new MostrarProyectos();
-$base->instancias();
-$resultado = $base->getProyectosId($idp);
-$resultado1 = $base->getIniProId($idp);
-$resultado2 = $base->getFinProId($idp);
+    function fecPro($idp){
+        $base = new MostrarProyectos();
+        $base->instancias();
+        $resultado1 = $base->getIniProId($idp);
+        $resultado2 = $base->getFinProId($idp);
+        $resultado = $base->getProyectosId($idp);
+        $nombre = $resultado[0]["NomProyecto"];
+        $objetivo = $resultado[0]["ObjPro"];
+        $monto = $resultado[0]["MontoPro"];
 
-//guardar los datos en variables
-$nombre = $resultado[0]["NomProyecto"];
+        $inicio = $resultado1[0]["IniPro"];
+        $fin = $resultado2[0]["FinPro"];
 
-$inicio = $resultado1[0]["IniPro"];
-$fin = $resultado2[0]["FinPro"];
-$objetivo = $resultado[0]["ObjPro"];
-$monto = $resultado[0]["MontoPro"];
-
+        return [$nombre, $objetivo, $monto,$inicio, $fin] ;
+    }
+    //hace la consulta principal de los datos de los proyectos
+    /*function datosPro($idp){
+        $base = new MostrarProyectos();
+        $base->instancias();
+        
+        
+        
+    }}
 
 ?>
-<script type='text/javascript'>
+<script languaje="javascript">
+    
+    document.getElementById("idp").value = "<?php echo $idp ?>";
+    document.getElementById("nom_proyecto").value = "<?php echo $nombre ?>";
+    document.getElementById("obj_proyecto").value = "<?php echo $objetivo ?>";
+    document.getElementById("monto_proyecto").value = "<?php echo $monto ?>";
+</script>
+<!--<script type='text/javascript'>
 
-    var fechaI = '<?php echo $inicio; ?>'
+    var fechaI = ''
     var fechaIni= new Date(fechaI);
     var mesI=fechaIni.getMonth()+1;
     var diaI=fechaIni.getDate()+1;
+ 
     var yearI=fechaIni.getFullYear();
     if(diaI<10)
         diaI='0'+diaI;
@@ -40,27 +52,28 @@ $monto = $resultado[0]["MontoPro"];
 </script>
 <script type='text/javascript'>
 
-    var fechaF = '<?php echo $fin; ?>'
+    var fechaF = '
     var fechaFin= new Date(fechaF);
+    console.log('Date in mexico1: ' + fechaFin);
+    let formatter = new Intl.DateTimeFormat('en-MEX', { timeZone: "America/Mexico_City" });   
+    let usDate = formatter.format(fechaFin);
+    console.log('Date in mexico: ' + usDate);
+
+    var diaF=fechaFin.getDate();
     var mesF=fechaFin.getMonth()+1;
-    var diaF=fechaFin.getDate()+1;
+
     var yearF=fechaFin.getFullYear();
     if(diaF<10)
         diaF='0'+diaF;
+   
     if(mesF<10)
         mesF='0'+mesF;
     
     document.getElementById("fin_proyecto").value =yearF+"-"+mesF+"-"+diaF;
 
-</script>
+</script>-->
 
 
 
-<!-- script para poner los valores en los campos correspondientes -->
-<script languaje="javascript">
-    
-    document.getElementById("idp").value = "<?php echo $idp ?>";
-    document.getElementById("nom_proyecto").value = "<?php echo $nombre ?>";
-    document.getElementById("obj_proyecto").value = "<?php echo $objetivo ?>";
-    document.getElementById("monto_proyecto").value = "<?php echo $monto ?>";
-</script>
+<!-- script para poner los valores en los campos correspondientes -->*/
+
