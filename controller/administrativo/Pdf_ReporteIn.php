be8a70762a20e2683235ffa1f23809bd37adb6d0
llamasdelacruz <tetillamas@gmail.com>
2023-04-29 14:44:08 -0600

    correcciones pdf y radio button

--- controller/administrativo/Pdf_ReporteIn.php
+++ controller/administrativo/Pdf_ReporteIn.php
@@ -27,9 +27,8 @@ class PDF extends FPDF{
         // Salto de línea
         $this->Ln(20);
         $this->Cell(280,10,'Colegio de Ingenieros en Sistemas Computacionales',0,1,'C');
-        $this->Ln();
         $this->Cell(280,10,'Reporte individual',0,1,'C');
-        $this->Ln();
+
         $this->Cell(60,10,$this->nombre_actividad,0,1,'L'); // Aqui el nombre de la actividad 
         $this->Cell(60,10,$this->periodo,0,1,'L'); //Aqui el periodo que se eligio 
     } 
@@ -41,7 +40,7 @@ class PDF extends FPDF{
 
         $this->SetFillColor(8,82,98);
         //Cabecera
-        $this->Ln(25);
+       
         //Color e la cabecarea de la tabla 
         foreach($header as $col)
         {
@@ -54,8 +53,11 @@ class PDF extends FPDF{
         $this->SetFont('Times','',12);
         $this->Ln();
         //crea las celdas de la tabla con los datos
-        for ($i=0; $i < count($filas) ; $i++) { 
-            
+        for ($i=0; $i < count($filas) ; $i++) {
+            //pone en negritas el texto 
+            if($i == (count($filas)-1)){
+                $this->SetFont('Times','B',13);
+            }
             $y = $this->GetY(); 
             $x = $this->GetX();
           
