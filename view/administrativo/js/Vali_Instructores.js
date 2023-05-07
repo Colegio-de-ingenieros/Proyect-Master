f40447a45376c1c84c63b1597449438e41b99a4e
TchRui <Rui099098@gmail.com>
2023-05-06 19:17:06 -0600

    Validaciones en reg_instructores

--- view/administrativo/js/Vali_Instructores.js
+++ view/administrativo/js/Vali_Instructores.js
@@ -1,22 +1,36 @@
+
 /* Expresiones regulares */
 const expresiones = {
     nombre: /^[a-zA-ZÁ-Ýá-ý\s.]{1,40}$/, // Letras y espacios, pueden llevar acentos.
     apellidos: /^[a-zA-ZÁ-Ýá-ý\s]{1,20}$/, // Letras y espacios, pueden llevar acentos.
+    especialidades: /^[a-zA-ZÁ-Ýá-ý\s]{1,60}$/, // Letras y espacios, pueden llevar acentos.
+    fecha: /^\d{4}-\d{2}-\d{2}$/, // Fecha en formato yyyy-mm-dd
+    hora: /^\d{2}:\d{2}$/, // Hora en formato hh:mm
+    texto_comas: /^[a-zA-ZÁ-Ýá-ý\s,.]{1,60}$/, // Letras y espacios, pueden llevar acentos y comas.
 }
-
+/* Conjunto de banderas para realizar verificación */
 const banderas = {
     nombre: false,
     paterno: false,
-    materno: false
+    materno: true,
+    especialidad: false
 }
+
+/* Conjunto de banderas para realizar verificación al añadir una certificación externa */
+const banderas_externas = {
+    nombre: false,
+    organizacion: false,
+    emision: false,
+    vencimiento: false,
+};
 
 /* Validación para el nombre */
 const nombre_campo = document.getElementById('nombre');
 nombre_campo.addEventListener('keyup', (e) => {
     let valorInput = e.target.value;
-    nombre_campo.value = valorInput.replace(/[üâäàåçê♪ëèïîìÄÅæ´°¨·ÆôöòûùÿÖÜ¢£¥₧ƒªº¿⌐¬½¼«»÷±~!¡@#$%^&^*()_+=\[\]{};':"\\|<>\/?-]/g, '');
+    nombre_campo.value = valorInput.replace(/[^a-zA-ZÁ-Ýá-ý\s.]/g, '');
 
-    if (!expresiones.nombres.test(valorInput)) {
+    if (!expresiones.nombre.test(valorInput)) {
         nombre_campo.style.border = "3px solid red";
         banderas.nombre = false;
     }
@@ -30,7 +44,7 @@ nombre_campo.addEventListener('keyup', (e) => {
 const paterno_campo = document.getElementById('paterno');
 paterno_campo.addEventListener('keyup', (e) => {
     let valorInput = e.target.value;
-    paterno_campo.value = valorInput.replace(/[üâäàåçê♪ëèïîìÄÅæ´°¨·ÆôöòûùÿÖÜ¢£¥₧ƒªº¿⌐¬½¼«»÷±~!¡@#$%^&^*()_+=\[\]{};':"\\|<>\/?-]/g, '');
+    paterno_campo.value = valorInput.replace(/[^a-zA-ZÁ-Ýá-ý\s]/g, '');
 
     if (!expresiones.apellidos.test(valorInput)) {
         paterno_campo.style.border = "3px solid red";
@@ -59,3 +73,53 @@ materno_campo.addEventListener('keyup', (e) => {
         }
     }
 });
+
+/* Validación para la especialidad */
+const especialidad_campo = document.getElementById('especialidad');
+especialidad_campo.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+    especialidad_campo.value = valorInput.replace(/[^a-zA-ZÁ-Ýá-ý\s]/g, '');
+
+    if (!expresiones.especialidades.test(valorInput)) {
+        especialidad_campo.style.border = "3px solid red";
+        banderas.especialidad = false;
+    }
+    else {
+        especialidad_campo.removeAttribute("style");
+        banderas.especialidad = true;
+    }
+});
+
+/* Validación para el nombre de la certificación externa */
+const nombre_certificacion_campo = document.getElementById('nombre-cert-externa');
+nombre_certificacion_campo.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+    /* Crea una expresión regular que tenga las siguientes especificaciónes: solo aceptará letras, espacios intermedios, números, la coma “,” y el punto “.” */
+    nombre_certificacion_campo.value = valorInput.replace(/[^a-zA-ZÁ-Ýá-ý\s,.]/g, '');
+
+    if (!expresiones.texto_comas.test(valorInput)) {
+        nombre_certificacion_campo.style.border = "3px solid red";
+        banderas_externas.nombre = false;
+    }
+    else {
+        nombre_certificacion_campo.removeAttribute("style");
+        banderas_externas.nombre = true;
+    }
+});
+
+/* Validacion para el campo de organización */
+const organizacion_campo = document.getElementById('organizacion-externa');
+organizacion_campo.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+    organizacion_campo.value = valorInput.replace(/[^a-zA-ZÁ-Ýá-ý\s.]/g, '');
+
+    if (!expresiones.nombre.test(valorInput)) {
+        organizacion_campo.style.border = "3px solid red";
+        banderas_externas.organizacion = false;
+    }
+    else {
+        organizacion_campo.removeAttribute("style");
+        banderas_externas.organizacion = true;
+    }
+});
+
