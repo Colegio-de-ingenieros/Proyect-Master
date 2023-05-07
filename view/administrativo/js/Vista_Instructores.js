0988740d68e7c679db983480c0657a2a6ae2bbb8
TchRui <Rui099098@gmail.com>
2023-05-06 22:01:00 -0600

    Generación de filas en tabla

--- view/administrativo/js/Vista_Instructores.js
+++ view/administrativo/js/Vista_Instructores.js
@@ -7,11 +7,50 @@ window.onload = function () {
         method: 'POST',
         body: datos
     })
-    .then((response) => response.json())
-    .then((json) => respuesta(json))
-    .catch((error) => alert(error));
+        .then((response) => response.json())
+        .then((json) => respuesta(json))
+        .catch((error) => alert(error));
 
     function respuesta(json) {
-        console.log(json);
+        const tableBody = document.querySelector("table tbody");
+
+        json.forEach(rowData => {
+            let row = document.createElement('tr');
+
+            // Nombre
+            let nameCell = document.createElement('td');
+            let nameText = rowData[1] + " " + rowData[2] + " " + (rowData[3] || "");
+            let nameTextNode = document.createTextNode(nameText);
+            nameCell.appendChild(nameTextNode);
+            row.appendChild(nameCell);
+
+            // Seguimiento
+            let seguimientoCell = document.createElement('td');
+            let seguimientoText = rowData[4];
+            let seguimientoTextNode = document.createTextNode(seguimientoText);
+            seguimientoCell.appendChild(seguimientoTextNode);
+            row.appendChild(seguimientoCell);
+
+            // Acciones
+            let accionesCell = document.createElement('td');
+            let verMasLink = document.createElement('a');
+            verMasLink.setAttribute('href', '../../view/administrativo/Ver_Instructor.html');
+            verMasLink.textContent = "Ver más";
+            accionesCell.appendChild(verMasLink);
+
+            let modificarLink = document.createElement('a');
+            modificarLink.setAttribute('href', '');
+            modificarLink.textContent = "Modificar";
+            accionesCell.appendChild(modificarLink);
+
+            let eliminarLink = document.createElement('a');
+            eliminarLink.setAttribute('href', '');
+            eliminarLink.textContent = "Eliminar";
+            accionesCell.appendChild(eliminarLink);
+
+            row.appendChild(accionesCell);
+
+            tableBody.appendChild(row);
+        });
     }
 };
