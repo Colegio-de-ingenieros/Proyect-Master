c103187e1383ece77a9260850b9c99201ca62516
ivan7351 <carlosivanmunoz3@gmail.com>
2023-05-06 21:08:41 -0600

    Visualización cuotas (empresa)

---
+++ model/empresa/Mostrar_Cuotas_empresa.php
@@ -0,0 +1,46 @@
+<?php
+include('../../config/Crud_bd.php'); 
+
+class MostrarCuota extends Crud_bd{
+
+
+    function usuario($correo){
+        $this->conexion_bd();
+        $consulta = "SELECT RFCUsuaEmp FROM usuaemp WHERE binary(CorreoUsuaEmp) =  binary(:user)";
+        $parametros = [":user"=>$correo];
+        $datos = $this->mostrar($consulta,$parametros);
+        $this->cerrar_conexion();
+        return $datos;
+    }
+
+    function cuotas_disponibles($id){
+        $this->conexion_bd();
+        $consulta = "SELECT MontoVigCuo, IniVigCuo, FinVigCuo,TipoCuota,DocCuota
+        FROM usuaemp,empvigcuota,vigenciacuotas,tipovigcuota,tipocuota
+        WHERE usuaemp.RFCUsuaEmp = '$id'
+        and usuaemp.RFCUsuaEmp = empvigcuota.RFCUsuaEmp
+        and empvigcuota.IdVigCuo = vigenciacuotas.IdVigCuo
+        and vigenciacuotas.IdVigCuo = tipovigcuota.IdVigCuo
+        and tipovigcuota.IdCuota = tipocuota.IdCuota";
+        $datos = $this->mostrar($consulta);
+        $this->cerrar_conexion();
+
+        return $datos;
+    }
+    
+    function buscar($busqueda,$id){
+        $this->conexion_bd();
+        $querry = "SELECT  MontoVigCuo, IniVigCuo, FinVigCuo,TipoCuota,DocCuota 
+        FROM usuaemp,empvigcuota,vigenciacuotas,tipovigcuota,tipocuota
+        WHERE usuaemp.RFCUsuaEmp = '$id'
+        and usuaemp.RFCUsuaEmp = empvigcuota.RFCUsuaEmp
+        and empvigcuota.IdVigCuo = vigenciacuotas.IdVigCuo
+        and vigenciacuotas.IdVigCuo = tipovigcuota.IdVigCuo
+        and tipovigcuota.IdCuota = tipocuota.IdCuota
+        and (MontoVigCuo LIKE :busqueda OR TipoCuota LIKE :busqueda)";
+        $resultados = $this->mostrar($querry, [":busqueda" => "%".$busqueda."%"]);
+        $this->cerrar_conexion();
+        return $resultados;
+    }
+}
+?>
