9a960c4c8cf366bfa91bcaaab98b8260de521c05
Luis Fernando <LZ20020098@zacatecassur.tecnm.mx>
2023-05-12 13:32:46 -0600

    .

--- model/socio-asociado/Modificar_Cuotas_socio.php
+++ model/socio-asociado/Modificar_Cuotas_socio.php
@@ -5,17 +5,11 @@
 
         public function actualizar($idV, $tipo, $monto, $inicio, $fin, $doc){
             $this->conexion_bd();
-
-            $consulta = "UPDATE tipovigcuota SET IdCuota=:nombre WHERE IdVigCuo=:idV";
-            $parametros = [":idV"=>$idV, ":nombre"=>$tipo];
             
             $consulta1 = "UPDATE vigenciacuotas SET MontoVigCuo=:monto, IniVigCuo=:inicio, FinVigCuo=:fin, DocCuota=:doc WHERE IdVigCuo=:idV";
             $parametros1 = [":idV"=>$idV, ":monto"=>$monto, ":inicio"=>$inicio, ":fin"=>$fin, ":doc"=>$doc];
             
-            $consul=[$consulta, $consulta1];
-            $para=[$parametros, $parametros1];
-            
-            $datos = $this->insertar_eliminar_actualizar($consul,$para);
+            $datos = $this->insertar_eliminar_actualizar($consulta1,$parametros1);
             $this->cerrar_conexion();
             return $datos;
         }
