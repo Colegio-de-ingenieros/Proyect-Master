1f9711e135d6ff470d9615751167937827e8cc0b
Retzat <ret.hatsu@gmail.com>
2023-05-14 15:37:26 -0600

    Trabajadores corregidos 50 percent

---
+++ controller/administrativo/Mostrar_Trabajadores.php
@@ -0,0 +1,190 @@
+<?php
+include_once('../../model/administrativo/Mostrar_Trabajadores.php');
+$salida = '';
+$base = new MostrarTrabajadores();
+$base->instancias();
+
+$rfc=0;
+
+if (isset($_POST['consulta'])) {
+    $busqueda = $_POST['consulta'];
+    $resultado = $base->buscador($busqueda);
+    if ($resultado == true) {
+        //pone los encabezados de la tabla
+        $salida .= '
+        <style>
+        table{
+            width: 150%;
+            border-collapse: collapse;
+            font-family: "Manrope";
+    
+        }
+        
+        .header_table thead th {
+            
+            top: 0;
+            background-color: #085262;
+            color: #e6e7e8;
+            font-size: 1.125rem;
+        }
+        th,td {
+            border-bottom: 1px solid #000000;
+            padding: 10px 20px;
+            font-size: 15px;
+            text-align: center;
+            background-color: #dfe3e7;
+        }
+        .di{
+           padding-top: 20px;
+        }
+        </style>
+        <div class="di">
+        <table class="header_table" >
+                        <thead  >
+                            <tr>
+                                <th>RFC</th>
+                                <th>Nombre</th>
+                                <th>Apellido paterno</th>
+                                <th>Apellido materno</th>
+                                <th>Correo electrónico</th>
+                                <th>Teléfono</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                <tbody>';
+    
+        //agrega los resultados de la busqueda
+        for ($i = 0; $i < count($resultado); $i++) {
+    
+            //obtiene los valores de la tupla actual de cada uno de los campos y los guarda como variables
+            $rfc = $resultado[$i]["RFCT"];
+            $nombre = $resultado[$i]["NombreT"];
+            $apat = $resultado[$i]["ApePT"];
+            $amat = $resultado[$i]["ApeMT"];
+            $correo = $resultado[$i]["CorreoT"];
+            $telefono = $resultado[$i]["TelT"];
+            //escribe los valores en la tabla
+            $salida .= '<tr>';
+            $salida .= '<td>' . $rfc. '</td>';
+            $salida .= '<td>' . $nombre . '</td>';
+            $salida .= '<td>' . $apat . '</td>';
+            $salida .= '<td>' . $amat . '</td>';
+            $salida .= '<td>' . $correo . '</td>';
+            $salida .= '<td>' . $telefono . '</td>';
+            $salida .= '<td><a href="../../controller/administrativo/Get_Trabajadores.php?rfc='.$rfc.'" >Modificar</a>&nbsp;&nbsp;&nbsp<a href="#" onclick="confirmDesactiv(String('.$id.'))" class="table_item__link">Eliminar</a></td>';
+            //
+            //
+            $salida .= '</tr></div>';
+    
+            
+        }
+    } 
+    
+    else {
+        $salida .= 'No se encontraron resultados';
+    }
+} else {
+    $resultado = $base->getTrabajadores();
+    if ($resultado == true) {
+        //pone los encabezados de la tabla
+        $salida .= '
+        <style>
+        table{
+            width: 150%;
+            border-collapse: collapse;
+            font-family: "Manrope";
+    
+        }
+        
+        .header_table thead th {
+            
+            top: 0;
+            background-color: #085262;
+            color: #e6e7e8;
+            font-size: 1.125rem;
+        }
+        th,td {
+            border-bottom: 1px solid #000000;
+            padding: 10px 20px;
+            font-size: 15px;
+            text-align: center;
+            background-color: #dfe3e7;
+        }
+        .di{
+           padding-top: 20px;
+        }
+        </style>
+        <div class="di">
+        <table class="header_table" >
+                        <thead  >
+                            <tr>
+                                <th>RFC</th>
+                                <th>Nombre</th>
+                                <th>Apellido paterno</th>
+                                <th>Apellido materno</th>
+                                <th>Correo electrónico</th>
+                                <th>Teléfono</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                <tbody>';
+    
+        //agrega los resultados de la busqueda
+        for ($i = 0; $i < count($resultado); $i++) {
+    
+            //obtiene los valores de la tupla actual de cada uno de los campos y los guarda como variables
+            $rfc = $resultado[$i]["RFCT"];
+            $nombre = $resultado[$i]["NombreT"];
+            $apat = $resultado[$i]["ApePT"];
+            $amat = $resultado[$i]["ApeMT"];
+            $correo = $resultado[$i]["CorreoT"];
+            $telefono = $resultado[$i]["TelT"];
+            //$extension = getExt($logo);
+    
+            //escribe los valores en la tabla
+            $salida .= '<tr>';
+            $salida .= '<td>' . $rfc. '</td>';
+            $salida .= '<td>' . $nombre . '</td>';
+            $salida .= '<td>' . $apat . '</td>';
+            $salida .= '<td>' . $amat . '</td>';
+            $salida .= '<td>' . $correo . '</td>';
+            $salida .= '<td>' . $telefono . '</td>';
+            $rfcc="'".$rfc."'";
+            $salida .= '<td><a href="../../controller/administrativo/Get_Trabajadores.php?rfc='.$rfc.'" >Modificar</a>&nbsp;&nbsp;&nbsp<a href="#" onclick="confirmDesactiv(String('.$rfcc.'))" class="table_item__link">Eliminar</a></td>';
+
+            $salida .= '</tr></div>';
+    
+            
+        }
+    } 
+    
+    else {
+        //echo "si entra al else";
+        $salida .= 'No se encontraron resultados';
+    }
+}
+
+
+
+$salida .= "</tbody></table>";
+echo '<script type="text/javascript">
+
+function confirmDesactiv(dato)
+{
+    var cadena="../../controller/administrativo/Eliminar_Trabajadores.php?rfc="+dato;
+   var flag = confirm("¿Está seguro de eliminar este trabajador?");
+   if(flag){
+        alert("Eliminado con éxito");
+        window.location.assign(cadena);
+        
+   }else
+        window.location.assign("Vista_trabajadores.php");
+}
+
+</script>';
+
+
+echo $salida;
+echo '<script src="../../controller/administrativo/js/Eliminar_Trabajadores_Confirmacion.js"></script>';
+?>
+<script src="../../controller/administrativo/js/Eliminar_Trabajadores_Confirmacion.js"></script>
---
+++ model/administrativo/Reg_Trabajadores.php
@@ -0,0 +1,68 @@
+<?php
+    include('../../config/Crud_bd.php');
+
+    class NuevoTrabajador{
+        private $base;
+
+        //crea un objeto del CRUD para hacer las consultas
+        function conexion(){
+            $this->base = new Crud_bd();
+            $this->base->conexion_bd();
+        }
+
+        //retorna true si el id que recibe ya esta en la base y false si no
+        function buscarPorRFC($rfc){
+            $querry = "SELECT * FROM trabajadores
+            WHERE RFCT = :rfc";
+            
+            
+            $arre = [":rfc"=>$rfc];
+
+            $resultados = $this->base->mostrar($querry, $arre);
+            
+            if($resultados != null){
+                
+                return true;
+            }
+
+            else{
+                return false;
+            }
+        }
+
+        //manda las consultas para insertar en las tablas de certificaciones internas e historicos
+        function insertar($nombre, $apat, $amat, $rfc, $correo, $telefono, $pass, $num){
+            //consultas para la tabla de certificaciones internas
+            $q1 = "INSERT INTO trabajadores (RFCT,NombreT,ApePT,ApeMT,CorreoT,TelT,ContraT)
+            VALUES(:rfc, :nombre, :apat, :amat, :correo, :telefono, :pass)";
+            $a1= [":rfc"=>$rfc, ":nombre"=>$nombre, ":apat"=>$apat, ":amat"=>$amat, ":correo"=>$correo, ":telefono"=>$telefono, ":pass"=>$pass];
+            //acomoda todo en arreglos para mandarlos al CRUD
+            $q2="INSERT INTO tratipousua (IdUsua,RFCT)
+            VALUES (:num, :rfc)";
+            $a2= [":num"=>$num,":rfc"=>$rfc];
+
+            $querry = [$q1,$q2];
+            $parametros = [$a1,$a2];           
+            
+            $this->base->insertar_eliminar_actualizar($querry, $parametros);
+            
+        }
+        function existeCorreo($correo)
+    {
+        # ve si el correo ya esta en la base
+        $dato1 = $this->base->mostrar("SELECT CorreoPerso FROM usuaperso WHERE binary(CorreoPerso)= binary(:correo)",[':correo'=>$correo]);
+        $dato2 = $this->base->mostrar("SELECT CorreoUsuaEmp FROM usuaemp WHERE binary(CorreoUsuaEmp)= binary(:correo)",[':correo'=>$correo]);
+        $dato3 = $this->base->mostrar("SELECT CorreoT FROM trabajadores WHERE binary(CorreoT)= binary(:correo)",[':correo'=>$correo]);
+
+        if(count($dato1) == 0 && count($dato2) == 0 && count($dato3) == 0){
+          
+            return false;
+        }else{
+            
+            return true;
+        }
+    }
+        
+    }
+
+?>
