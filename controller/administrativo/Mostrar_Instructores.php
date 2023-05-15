a25fc8acfb2ba6eaa10e4036719dc97a61a09a85
llamasdelacruz <tetillamas@gmail.com>
2023-05-15 14:35:57 -0600

    busquerda y mostrar einstructores funcional

---
+++ controller/administrativo/Mostrar_Instructores.php
@@ -0,0 +1,20 @@
+<?php
+include_once('../../model/administrativo/Mostrar_Instructores.php');
+
+$objeto = new Mostrar_Instructor();
+
+    $instructores = $objeto->extraerInstructores();
+
+    $lista_instructores = [];
+    foreach ($instructores as $element) {
+        $lista_instructores[] = [
+            $element['ClaveIns'],
+            $element['NomIns'],
+            $element['ApePIns'],
+            $element['ApeMIns'],
+            $element['EstatusIns']
+        ];
+    }
+
+    echo json_encode($lista_instructores);
+?>
