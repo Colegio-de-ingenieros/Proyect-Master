45d092ea64a67ed7957511d2b438b90cb37327d5
FanySaLu <fanylulo@hotmail.com>
2023-05-17 08:03:31 -0600

    .

---
+++ view/administrativo/js/Cargar_Logo.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+    //--------------------- SELECCIONAR FOTO PRODUCTO ---------------------
+    $("#inputLogo").on("change",function(){
+    	var uploadFoto = document.getElementById("inputLogo").value;
+        var foto       = document.getElementById("inputLogo").files;
+        var nav = window.URL || window.webkitURL;
+        var contactAlert = document.getElementById('form_alert');
+        
+            if(uploadFoto !='')
+            {
+                var type = foto[0].type;
+                var name = foto[0].name;
+                if(type != 'image/jpeg' && type != 'image/jpg' && type != 'image/png' && type != 'image/gif')
+                {
+                    contactAlert.innerHTML = '<p class="errorArchivo">El archivo no es válido.</p>';                        
+                    $("#img").remove();
+                    $(".delPhoto").addClass('notBlock');
+                    $('#inputLogo').val('');
+                    return false;
+                }else{  
+                        contactAlert.innerHTML='';
+                        $("#img").remove();
+                        $(".delPhoto").removeClass('notBlock');
+                        var objeto_url = nav.createObjectURL(this.files[0]);
+                        $(".prevPhoto").append("<img id='img' src="+objeto_url+">");
+                        $(".upimg label").remove();
+                        
+                    }
+              }else{
+              	alert("No selecciono foto");
+                $("#img").remove();
+              }              
+    });
+
+    $('.delPhoto').click(function(){
+    	$('#inputLogo').val('');
+    	$(".delPhoto").addClass('notBlock');
+    	$("#img").remove();
+
+    });
+
+});
