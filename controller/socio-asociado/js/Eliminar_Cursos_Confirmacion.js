df2cabb05ed0629181b471734bd78f737119c093
Luis Fernando <LZ20020098@zacatecassur.tecnm.mx>
2023-05-17 11:35:11 -0600

    .

--- controller/socio-asociado/js/Eliminar_Cursos_Confirmacion.js
+++ controller/socio-asociado/js/Eliminar_Cursos_Confirmacion.js
@@ -15,7 +15,7 @@ function confirmacion(e){
                 // Procesar la respuesta del servidor en caso de éxito
                 alert('Eliminado con éxito');
                 // volver a la pagina de vista
-                location.href = '../../view/socio-asociado/Ver_Cursos.html';
+                location.href = '../../view/socio-asociado/Vista_Cursos.html';
             },
         });
 
