6cca5d0ce4cf872f9f5489cd1130871f994a9fad
Retzat <ret.hatsu@gmail.com>
2023-05-19 08:11:05 -0600

    Revert "correcion validacion"
    This reverts commit 66cd2bdfd999a318e525f5b1d29358f53f78fd26.

--- view/administrativo/js/Vali_Oferta_AR.js
+++ view/administrativo/js/Vali_Oferta_AR.js
@@ -11,8 +11,6 @@ textBox.disabled = true;
 radioButton1.addEventListener("change", function() {
     if (radioButton1.checked) {
       textBox.disabled = true;
-      descri_puesto.removeAttribute("style");
-      document.getElementById("descri_puesto").value = "";
       guardar.disabled=false;
     }
   });
