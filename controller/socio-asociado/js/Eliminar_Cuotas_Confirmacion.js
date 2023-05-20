3ed1248643540057f30df8e0793ef95ee5fcb183
Luis Fernando <LZ20020098@zacatecassur.tecnm.mx>
2023-05-20 15:43:45 -0600

    .

--- controller/socio-asociado/js/Eliminar_Cuotas_Confirmacion.js
+++ controller/socio-asociado/js/Eliminar_Cuotas_Confirmacion.js
@@ -13,7 +13,8 @@ function confirmacion(e){
             success: function (response)
             {
                 // Procesar la respuesta del servidor en caso de éxito
-                alert('Eliminado con éxito');
+                alert(response);
+                //alert('Eliminado con éxito');
                 // volver a la pagina de vista
                 location.href = '../../view/socio-asociado/Vista_Cuotas.html';
             },
