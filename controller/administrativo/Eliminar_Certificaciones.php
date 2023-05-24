b18a7972d0ebd07724652de460faeda87a61f4a2
Ana-Isabel-Garcia <cuentaderespaldogit@gmail.com>
2023-05-24 08:29:22 -0600

    muestreo de errores en el server

--- controller/administrativo/Eliminar_Certificaciones.php
+++ controller/administrativo/Eliminar_Certificaciones.php
@@ -8,7 +8,10 @@ $obj->instanciar();
 //verifica si hay seguimientos de la certificacion, simulamos un error 403
 $seg = $obj->buscarSeg($idc);
 
+echo $seg . '<br>';
+
 if($seg == true){
+    echo '  relacion con seguimiento';
     http_response_code(403);
 }
 
@@ -16,12 +19,16 @@ else{
     //verifica si hay relacion con instructores, simulamos un error 405
     $ins = $obj->buscarIns($idc);
 
+    echo $inst . '<br>';
+
     if($ins == true){
         //si hay relacion
+        echo '  relacion con instructores';
         http_response_code(405);
     }
 
     else{
+        echo '  no hay relacion';
         //no hay relacion, se elimina la certificacion y el proceso termina sin errores
         $obj->eliminar($idc);
     }
