87ef55dc7e52ea07e2cfcfb9ef11f518b513d3ec
llamasdelacruz <tetillamas@gmail.com>
2023-08-29 17:18:29 -0600

    correccion perfil empresa

--- controller/empresa/Mostrar_Datos_Perfil_Empresa.php
+++ controller/empresa/Mostrar_Datos_Perfil_Empresa.php
@@ -1,9 +1,10 @@
 <?php
+require_once("../../model/empresa/Mostrar_Datos_Perfil_Empresa.php");
 session_start();
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-require_once("../../model/empresa/Mostrar_Datos_Perfil_Empresa.php");
+
 
 $objeto = new Mostrar_perfil_empresa();
 $datos = [];
