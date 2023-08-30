1451633f048f5f9fa71cd113ff213ae3c093b62b
llamasdelacruz <tetillamas@gmail.com>
2023-08-29 22:11:46 -0600

    comentar lineas para error

--- controller/empresa/Mostrar_Datos_Perfil_Empresa.php
+++ controller/empresa/Mostrar_Datos_Perfil_Empresa.php
@@ -1,9 +1,9 @@
 <?php
 require_once("../../model/empresa/Mostrar_Datos_Perfil_Empresa.php");
 session_start();
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
 
 
 $objeto = new Mostrar_perfil_empresa();
