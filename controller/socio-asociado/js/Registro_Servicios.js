7758ce995662b214476a55672a4b189ec1baaeae
TchRui <Rui099098@gmail.com>
2023-09-29 10:22:37 -0600

    Cambio de boton en servicios

--- controller/socio-asociado/js/Registro_Servicios.js
+++ controller/socio-asociado/js/Registro_Servicios.js
@@ -1,97 +1,100 @@
-const check_headhunter = document.getElementById('headhunter');
-let headhunter = 0;
+window.onload = () => {
+  const check_headhunter = document.getElementById('headhunter');
+  let headhunter = 0;
 
-const check_outplacement = document.getElementById('outplacement');
-let outplacement = 0;
+  const check_outplacement = document.getElementById('outplacement');
+  let outplacement = 0;
 
-const btn_registrar = document.getElementById('btn-aplicar');
+  const btn_registrar = document.getElementById('btn-aplicar');
 
-check_headhunter.addEventListener('change', (event) => {
-  if (event.target.checked) {
-    headhunter = 1;
-    console.log(headhunter);
-  } else {
-    headhunter = 0;
-    console.log(headhunter);
-  }
-});
-
-check_outplacement.addEventListener('change', (event) => {
-  if (event.target.checked) {
-    outplacement = 1;
-    console.log(outplacement);
-  } else {
-    outplacement = 0;
-    console.log(outplacement);
-  }
-});
-
-btn_registrar.addEventListener('click', () => {
-  const date = new Date();
-
-  if (headhunter == 0 && outplacement == 0) {
-    alert('Debe seleccionar al menos un servicio');
-  }
-  else if (headhunter == 1 && outplacement == 1) {
-    let dd = date.getDate();
-    let mm = date.getMonth() + 1;
-    let yyyy = date.getFullYear();
-
-    if (dd < 10) {
-      dd = '0' + dd;
+  check_headhunter.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      headhunter = 1;
+      console.log(headhunter);
+    } else {
+      headhunter = 0;
+      console.log(headhunter);
     }
-    if (mm < 10) {
-      mm = '0' + mm;
+  });
+
+  check_outplacement.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      outplacement = 1;
+      console.log(outplacement);
+    } else {
+      outplacement = 0;
+      console.log(outplacement);
     }
-    let fecha = yyyy + '-' + mm + '-' + dd;
+  });
 
-    let url = "../../controller/socio-asociado/Registro_Servicios.php";
-    let form = new FormData();
-    form.append("headhunter", headhunter);
-    form.append("outplacement", outplacement);
-    form.append("fecha", fecha);
+  btn_registrar.addEventListener('click', () => {
+    const date = new Date();
 
-    fetch(url, {
-      method: 'POST',
-      body: form
-    }).then(res => res.json())
-      .catch(error => console.error('Error:', error))
-      .then(response => {
-        alert("Envío exitoso")
-        check_headhunter.checked = false;
-        check_outplacement.checked = false;
-      });
-  }
-  else if (headhunter == 1 || outplacement == 1) {
-    let dd = date.getDate();
-    let mm = date.getMonth() + 1;
-    let yyyy = date.getFullYear();
-
-    if (dd < 10) {
-      dd = '0' + dd;
+    if (headhunter == 0 && outplacement == 0) {
+      alert('Debe seleccionar al menos un servicio');
     }
-    if (mm < 10) {
-      mm = '0' + mm;
+    else if (headhunter == 1 && outplacement == 1) {
+      let dd = date.getDate();
+      let mm = date.getMonth() + 1;
+      let yyyy = date.getFullYear();
+
+      if (dd < 10) {
+        dd = '0' + dd;
+      }
+      if (mm < 10) {
+        mm = '0' + mm;
+      }
+      let fecha = yyyy + '-' + mm + '-' + dd;
+
+      let url = "../../controller/socio-asociado/Registro_Servicios.php";
+      let form = new FormData();
+      form.append("headhunter", headhunter);
+      form.append("outplacement", outplacement);
+      form.append("fecha", fecha);
+
+      fetch(url, {
+        method: 'POST',
+        body: form
+      }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => {
+          alert("Envío exitoso")
+          check_headhunter.checked = false;
+          check_outplacement.checked = false;
+        });
     }
-    let fecha = yyyy + '-' + mm + '-' + dd;
+    else if (headhunter == 1 || outplacement == 1) {
+      let dd = date.getDate();
+      let mm = date.getMonth() + 1;
+      let yyyy = date.getFullYear();
 
-    let url = "../../controller/socio-asociado/Registro_Servicios.php";
+      if (dd < 10) {
+        dd = '0' + dd;
+      }
+      if (mm < 10) {
+        mm = '0' + mm;
+      }
+      let fecha = yyyy + '-' + mm + '-' + dd;
 
-    let form = new FormData();
-    form.append("headhunter", headhunter);
-    form.append("outplacement", outplacement);
-    form.append("fecha", fecha);
+      let url = "../../controller/socio-asociado/Registro_Servicios.php";
 
-    fetch(url, {
-      method: 'POST',
-      body: form
-    }).then(res => res.json())
-      .catch(error => console.error('Error:', error))
-      .then(response => {
-        alert("Envío exitoso");
-        check_headhunter.checked = false;
-        check_outplacement.checked = false;
-      });
-  }
+      let form = new FormData();
+      form.append("headhunter", headhunter);
+      form.append("outplacement", outplacement);
+      form.append("fecha", fecha);
 
-});
+      fetch(url, {
+        method: 'POST',
+        body: form
+      }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => {
+          alert("Envío exitoso");
+          check_headhunter.checked = false;
+          check_outplacement.checked = false;
+        });
+    }
+
+  });
+
+};
