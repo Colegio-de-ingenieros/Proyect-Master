7ba03d6138113ea79a1149fbe8644a3d6584a626
Retzat <lz200200064@zacatecassur.tecnm.mx>
2023-10-02 21:37:13 -0600

    Tabla polizas

--- controller/administrativo/Mostrar_Polizas.php
+++ controller/administrativo/Mostrar_Polizas.php
@@ -11,4 +11,53 @@ if($valTipo!=""){
         $resultado = $objeto->Mostrar_Ingresos();
     }
 }
-echo $valTipo;
+if (isset($_POST['consulta'])){
+    $busqueda=($_POST['consulta']);
+    if ($valTipo=="egreso"){
+        $resultado = $objeto->Mostrar_Egresos();
+    }else if ($valTipo=="ingreso"){
+        $resultado = $objeto->Mostrar_Ingresos();
+    }
+}
+if ($resultado == true) {
+    //pone los encabezados de la tabla
+    $salida .= '<table>
+                    <thead>
+                        <tr>
+                            <th>Folio</th>
+                            <th>Concepto general</th>
+                            <th>Fecha</th>
+                            <th>Elaboro</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                <tbody>';
+
+    //agrega los resultados de la busqueda
+    for ($i = 0; $i < count($resultado); $i++) {
+            //$auxValTipo="Curso";
+            $folio= $resultado[$i]["IdPolGral"];
+            $elaboro = $resultado[$i]["NomElaPol"].' '.$resultado[$i]["ApePElaPol"].' '.$resultado[$i]["ApeMElaPol"];
+            $concepto = $resultado[$i]["CoceptoGral"];
+            $fecha = $resultado[$i]["FechaPolGral"];
+
+            //escribe los valores en la tabla
+            $salida .= '<tr>';
+            $salida .= '<td>' . $folio . '</td>';
+            $salida .= '<td>' . $concepto . '</td>';
+            $salida .= '<td>' . $fecha . '</td>';
+            $salida .= '<td>' . $elaboro . '</td>';
+            $salida .= '<td>  <a href="../../view/administrativo/Vista_Datos_Usuario.html?usuario='.$folio.'">Ver más</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="../../view/administrativo/Vista_Cursos_SocioAsociado.html?id='.$folio.'">Cursos</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="../../view/administrativo/Vista_Cuotas_SocioAsociado.html?id='.$folio.'">Cuotas</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        </td>';
+            $salida .= '</tr>';  
+    }
+} 
+else {
+    $salida .= 'No se encontraron resultados';
+}
+
+echo $salida;
+
+?>
--- controller/administrativo/js/Mostrar_Polizas.js
+++ controller/administrativo/js/Mostrar_Polizas.js
@@ -1,61 +1,39 @@
-/* let tipo;
-window.onload = function () {
-    document.getElementById("radio_ingreso").checked=false;
-    document.getElementById("egreso").checked=false;
-    tipo="egreso";
-    $(buscar_datos());
-}
+window.onload = () => {
+  const ingreso = document.getElementById("radio_ingreso");
+  const egreso = document.getElementById("egreso");
+  let tipo;
+  ingreso.addEventListener("click", function () {
+    tipo = "ingreso";
+    buscar_datos(tipo);
+  });
 
-const radioButtons = document.querySelectorAll('input[name="radio"]');
-for (const radioButton of radioButtons) {
-  radioButton.addEventListener('change', verSeleccion);
-  
-}
+  egreso.addEventListener("click", function () {
+    tipo = "egreso";
+    buscar_datos(tipo);
+  });
 
-function verSeleccion(e){
-    if (this.checked) {
-        tipo=this.value
-        console.log(tipo);
-        $(buscar_datos());
-    }
-}
-function buscar_datos(consulta){
+  const buscar_datos = (consulta) => {
     $.ajax({
-        url: '../../controller/administrativo/Mostrar_Polizas.php',
-        type: 'POST',
-        dataType: 'html',
-        data: { consulta: consulta,  tipo:tipo},
-    })
-
-        .done(function (respuesta)
-        {
-            $("#tablaResultado").html(respuesta);
+        url: "../../controller/administrativo/Mostrar_Polizas.php",
+        type: "POST",
+        dataType: "html",
+        data: { consulta: consulta, tipo: tipo },
+      })
+  
+        .done(function (respuesta) {
+          $("#tablaResultado").html(respuesta);
         })
-        .fail(function ()
-        {
-            console.log("error");
-        })
-}
-
-$(document).on('keyup', '#busqueda', function (){
+        .fail(function () {
+          console.log("error");
+        });
+  };
+  $(document).on('keyup', '#busqueda', function (){
     var valorBusqueda = $(this).val();
+    console.log(valorBusqueda);
     if (valorBusqueda != "") {
         buscar_datos(valorBusqueda);
     } else {
         buscar_datos();
     }
-}) */
-
-window.onload = () => {
-  const ingreso = document.getElementById("radio_ingreso");
-  const egreso = document.getElementById("egreso");
-  let tipo;
-  
-  ingreso.addEventListener("click", function () {
-    tipo="ingreso";
-  });
-
-  egreso.addEventListener("click", function () {
-    tipo="egreso";
-  });
+})
 };
