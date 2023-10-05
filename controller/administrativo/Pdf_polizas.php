25034ff1d308ad6af04855e7ca81a23166eaa800
TchRui <Rui099098@gmail.com>
2023-10-04 22:26:25 -0600

    Estandarización de medidas

--- controller/administrativo/Pdf_polizas.php
+++ controller/administrativo/Pdf_polizas.php
@@ -19,72 +19,52 @@
         }
 
         function NbLines($w, $txt) {
-            $anchoCaracteres = &$this->CurrentFont['cw'];
-
-            if ($anchoCelda == 0) {
-                $anchoCelda = $this->w - $this->rMargin - $this->x;
+            $cw = &$this->CurrentFont['cw'];
+            if ($w == 0) {
+                $w = $this->w - $this->rMargin - $this->x;
             }
-
-            $anchoMaximoLinea = ($anchoCelda - 2 * $this->cMargin) * 1000 / $this->FontSize;
-            $textoSinRetornoCarro = str_replace("\r", '', $texto);
-            $longitudTexto = strlen($textoSinRetornoCarro);
-
-            if ($longitudTexto > 0 && $textoSinRetornoCarro[$longitudTexto - 1] == "\n") {
-                $longitudTexto--;
+            $wmax = ($w - 2 * $this->cMargin) * 1000 / $this->FontSize;
+            $s = str_replace("\r", '', $txt);
+            $nb = strlen($s);
+            if ($nb > 0 && $s[$nb - 1] == "\n") {
+                $nb--;
             }
-
-            $posicionUltimoEspacio = -1;
-            $posicionActual = 0;
-
-            $inicioLineaActual = 0;
-            $longitudLineaActual = 0;
-
-            $numeroTotalLineas = 1;
-
-            while ($posicionActual < $longitudTexto) {
-
-                $caracterActual = $textoSinRetornoCarro[$posicionActual];
-
-                if ($caracterActual == "\n") {
-                    $posicionActual++;
-                    $posicionUltimoEspacio = -1;
-                    $inicioLineaActual = $posicionActual;
-                    $longitudLineaActual = 0;
-                    $numeroTotalLineas++;
+            $sep = -1;
+            $i = 0;
+            $j = 0;
+            $l = 0;
+            $nl = 1;
+            while ($i < $nb) {
+                $c = $s[$i];
+                if ($c == "\n") {
+                    $i++;
+                    $sep = -1;
+                    $j = $i;
+                    $l = 0;
+                    $nl++;
                     continue;
                 }
-
-                if ($caracterActual == ' ') {
-                    $posicionUltimoEspacio = $posicionActual;
+                if ($c == ' ') {
+                    $sep = $i;
                 }
-
-                $longitudLineaActual += $anchoCaracteres[$caracterActual];
-
-                if ($longitudLineaActual > $anchoMaximoLinea) {
-
-                    if ($posicionUltimoEspacio == -1) {
-
-                        if ($posicionActual == $inicioLineaActual) {
-                            $posicionActual++;
+                $l += $cw[$c];
+                if ($l > $wmax) {
+                    if ($sep == -1) {
+                        if ($i == $j) {
+                            $i++;
                         }
-
-                    } 
-                    else {
-                        $posicionActual = $posicionUltimoEspacio + 1;
+                    } else {
+                        $i = $sep + 1;
                     }
-
-                    $posicionUltimoEspacio = -1;
-                    $inicioLineaActual = $posicionActual;
-
-                    $longitudLineaActual = 0;
-                    $numeroTotalLineas++;
-
-                } 
-                else {
-                    $posicionActual++;
+                    $sep = -1;
+                    $j = $i;
+                    $l = 0;
+                    $nl++;
+                } else {
+                    $i++;
                 }
             }
-            return $numeroTotalLineas;
+            return $nl;
         }
 
         // Pie de página
@@ -134,73 +114,93 @@
     $response_name_service = $bd -> TituloServicio($tipo_servicio, $id);
     $titulo_servicio = $response_name_service[0]['Nombre_servicio'];
 
-    $secciones = 0;
-    
 
     $pdf = new PDF();
+
+    //* Definimos el tamaño de la ventana y la altura de las celdas
+    $ancho_total = $pdf -> GetPageWidth() - 20;
+    $altura = 10;
+
     $pdf -> AliasNbPages();
     $pdf -> AddPage();
     $pdf -> SetFillColor(255,255,255);
 
     $pdf -> SetFont('Arial','B',16);
-    $pdf -> Cell(0,10,utf8_decode('Póliza de '.$tipo_poliza.'s'),0,1,'C');
+    $pdf -> Cell(0,$altura,utf8_decode('Póliza de '.$tipo_poliza.'s'),0,1,'C');
 
     $pdf -> SetFont('Arial','B',13);
-    $pdf -> Cell(0,10,utf8_decode('Datos Generales'),0,1,'L');
+    $pdf -> Cell(0,$altura,utf8_decode('Datos Generales'),0,1,'L');
 
     $pdf -> SetFont('Arial','',12);
-    $pdf -> Cell(0,10,utf8_decode('Nombre: '.$nombre),0,1,'L');
-    $pdf -> Cell(0,10,utf8_decode('Tipo de servicio: '.$tipo_servicio),0,1,'L');
-    $pdf -> Cell(0,10,utf8_decode('Nombre del servicio: '.$titulo_servicio),0,1,'L');
+    $pdf -> Cell(0,$altura,utf8_decode('Nombre: '.$nombre),0,1,'L');
+    $pdf -> Cell(0,$altura,utf8_decode('Tipo de servicio: '.$tipo_servicio),0,1,'L');
+    $pdf -> Cell(0,$altura,utf8_decode('Nombre del servicio: '.$titulo_servicio),0,1,'L');
 
 
     $pdf -> SetFont('Arial','B',13);
-    $pdf -> Cell(0,10,utf8_decode('Datos de la póliza'),0,1,'L');
+    $pdf -> Cell(0,$altura,utf8_decode('Datos de la póliza'),0,1,'L');
 
     //* TABLA DE DATOS DE LA PÓLIZA
     $pdf -> SetFont('Arial','B',13);
     $pdf -> SetFillColor(8, 82, 98);
     $pdf -> SetTextColor(255,255,255);
 
-    $pdf -> Cell(0,10,utf8_decode('Póliza de '.$tipo_poliza),1,1,'C',true);
+    $pdf -> Cell(0,$altura,utf8_decode('Póliza de '.$tipo_poliza),1,1,'C',true);
     $pdf -> SetFont('Arial','B',12);
-    $pdf -> Cell(47.5,10,utf8_decode('Fecha'),1,0,'C',true);
+
+    //* SECCIÓN FECHA Y FOLIO
+    $ancho_celda = $ancho_total / 4;
+    $pdf -> Cell($ancho_celda,$altura,utf8_decode('Fecha'),1,0,'C',true);
 
     //Celdas blancas
     $pdf -> SetFont('Arial','',12);
     $pdf -> SetFillColor(255, 255, 255);
     $pdf -> SetTextColor(0,0,0);
-    $pdf -> Cell(47.5,10,utf8_decode($fecha),1,0,'C',true);
+    $pdf -> Cell($ancho_celda,$altura,utf8_decode($fecha),1,0,'C',true);
 
     //Celdas azules
     $pdf -> SetFont('Arial','B',12);
     $pdf -> SetFillColor(8, 82, 98);
     $pdf -> SetTextColor(255,255,255);
-    $pdf -> Cell(47.5,10,utf8_decode('Folio'),1,0,'C',true);
+    $pdf -> Cell($ancho_celda,$altura,utf8_decode('Folio'),1,0,'C',true);
 
     //Celdas blancas y salto de celdas
     $pdf -> SetFont('Arial','',12);
     $pdf -> SetFillColor(255, 255, 255);
     $pdf -> SetTextColor(0,0,0);
-    $pdf -> Cell(47.5,10,utf8_decode($id),1,1,'C',true);
+    $pdf -> Cell($ancho_celda,$altura,utf8_decode($id),1,1,'C',true);
 
     $pdf->SetFont('Arial','',12);
-    // Calcular el número de líneas necesarias para el texto
     $lineas = $pdf->NbLines(142.5, $concepto);
-    // Calcular la altura necesaria para la multicelda
     $altura = 5 * $lineas;
 
-    // Celda verde
-    $pdf->SetFont('Arial','B',12);
-    $pdf->SetFillColor(8, 82, 98);
-    $pdf->SetTextColor(255,255,255);
-    $pdf->Cell(47.5, $altura, utf8_decode('Concepto general'), 1, 0, 'C', true);
+    //* SECCION CONCEPTO GENERAL
+    // Celda azul
+    $pdf -> SetFont('Arial','B',12);
+    $pdf -> SetFillColor(8, 82, 98);
+    $pdf -> SetTextColor(255,255,255);
+    $pdf -> Cell($ancho_celda, $altura, utf8_decode('Concepto general'), 1, 0, 'C', true);
 
     // Celda blanca
-    $pdf->SetFont('Arial','',12);
-    $pdf->SetFillColor(255, 255, 255);
-    $pdf->SetTextColor(0,0,0);
-    $pdf->MultiCell(142.5, 5, utf8_decode($concepto), 1);
+    $pdf -> SetFont('Arial','',12);
+    $pdf -> SetFillColor(255, 255, 255);
+    $pdf -> SetTextColor(0,0,0);
+    $pdf -> MultiCell($ancho_celda * 3, 5, utf8_decode($concepto), 1);
+
+    //* SECCION CABECERAS DE LA TABLA CONCEPTO | DEBE | HABER | DESCRIPCION | COMPROBANTE
+    //Celdas de color gris
+    $pdf -> SetFont('Arial','B',12);
+    $pdf -> SetTextColor(0,0,0);
+    $pdf -> SetFillColor(223, 227, 231);
+
+    $ancho_celda = $ancho_total / 5;
+    $altura = 10;
+
+    $pdf -> Cell($ancho_celda, $altura,utf8_decode('Concepto'),1,0,'C',true);
+    $pdf -> Cell($ancho_celda, $altura,utf8_decode('Debe'),1,0,'C',true);
+    $pdf -> Cell($ancho_celda, $altura,utf8_decode('Haber'),1,0,'C',true);
+    $pdf -> Cell($ancho_celda, $altura,utf8_decode('Descripción'),1,0,'C',true);
+    $pdf -> Cell($ancho_celda, $altura,utf8_decode('Comprobante'),1,1,'C',true);
     
     $pdf -> Output();
 ?>
